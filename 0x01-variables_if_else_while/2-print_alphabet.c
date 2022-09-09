a42e3e0dcd9b9d3a342c23ea21770a8f5b10c2bc
Adejori Eniola <enx.co21@gmail.com>
2022-09-09 07:03:06 -0700

    task 3

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+/**
+* main - Entry point
+*
+* Return: alphabets
+*/
+
+int main(void)
+{
+	int a = 97;
+	while ( a < 123 )
+	{
+		putchar(a);
+		a++;
+	}
+	putchar(10);
+	return (0);
+}
+
