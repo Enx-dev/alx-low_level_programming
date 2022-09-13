d6c7033472ac90ba9a4daff9a1983feace432763
Adejori Eniola <enx.co21@gmail.com>
2022-09-13 08:35:32 -0700

    update

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* print_last_digit - prints last digit of num
+*
+* @num: interger value
+*
+* Return: last digit of num
+*/
+
+int print_last_digit(int num)
+{
+	int last;
+
+	last = num % 10;
+	return (last);
+}
