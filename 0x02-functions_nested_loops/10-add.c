f885cad371fcf3ac85530bbcd56f16c84274d0cb
Adejori Eniola <enx.co21@gmail.com>
2022-09-13 10:20:06 -0700

    update

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* add - adds num1 to num2
+* @num1: number
+* @num2: numer
+*
+* Return: number
+*/
+
+int add(int num1, int num2)
+{
+	int sum;
+
+	sum = num1 + num2;
+
+	return (sum);
+}
