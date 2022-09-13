f1b94409099f367d86827120ef9cab6d1a32aa5a
Adejori Eniola <enx.co21@gmail.com>
2022-09-13 10:12:28 -0700

    update

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+* times_table - someting
+*/
+
+void times_table(void)
+{
+	int num = 0;
+	int mult = 1;
+	int prod;
+
+	while (num <= 9)
+	{
+		_putchar('0');
+		while (mult <= 9)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			prod = num * mult;
+
+			if (prod <= 9)
+				_putchar(' ');
+			else
+				_putchar((prod / 10) + '0');
+
+			_putchar((prod & 10) + '0');
+			mult++;
+		}
+		mult = 1;
+		_putchar('\n');
+		num++;
+	}
+}
