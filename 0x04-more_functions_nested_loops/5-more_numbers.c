57e5d4a5db564730cd65a56cbba4b1f8ba8e5269
Adejori Eniola <enx.co21@gmail.com>
2022-09-15 06:36:30 -0700

    update

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* more_numbers - prints numbers from 0 t0 14 ten times
+* Return: void
+*/
+
+void more_numbers(void)
+{
+	int row = 0;
+	int columns = 0;
+
+	while (row < 10)
+	{
+		while (columns <= 14)
+		{
+			_putchar(columns + '0');
+			columns++;
+		}
+		columns = 0;
+		_putchar('\n');
+		row++;
+	}
+}
