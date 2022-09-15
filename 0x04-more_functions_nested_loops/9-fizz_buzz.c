e12db24f62d2bfc50cd0db32bbfeab5e96e666d1
Adejori Eniola <enx.co21@gmail.com>
2022-09-15 09:34:08 -0700

    update

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -7,21 +7,24 @@
 
 int main(void)
 {
-	int start = 0;
+	int start = 1;
 
-	while (start < 100)
+	while (start <= 100)
 	{
-		start++;
-		if (start % 3 == 0 && start % 5 == 0)
+		if ((start % 3) == 0 && (start % 5) == 0)
 			printf("FizzBuzz");
-		else if (start % 3 == 0)
+		else if ((start % 3) == 0)
 			printf("Fizz");
-		else if (start % 5 == 0)
+		else if ((start % 5) == 0)
 			printf("Buzz");
 		else
-		{
 			printf("%d", start);
+		if (start == 100)
+		{
+			start++;
+			continue;
 		}
+		start++;
 		printf(" ");
 
 	}
