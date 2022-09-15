b185d2caceb32c216a04caa73e4304e2c0421107
Adejori Eniola <enx.co21@gmail.com>
2022-09-15 05:49:44 -0700

    update

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* mul - multiplies @a with @b
+* @a: a number
+* @b: a number
+* Return: Product of multiplication
+*/
+
+int mul(int a, int b);
+{
+	int product;
+
+	product = a * b;
+	return (product);
+}
