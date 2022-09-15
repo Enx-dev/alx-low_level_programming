0db99c24481980e644dc156aed8eec74f559f3de
Adejori Eniola <enx.co21@gmail.com>
2022-09-15 10:05:26 -0700

    update

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+* print_triangle - prints a triangle
+* @size: number
+*/
+
+void print_triangle(int size)
+{
+	int h = 1;
+	int p;
+	int s;
+
+	if (size > 0)
+	{
+		while (h <= size)
+		{
+			p = 0;
+			s = 0;
+
+			while (s <= (size - h))
+			{
+				_putchar(' ');
+				s++;
+			}
+			while (p < h)
+			{
+				_putchar('#');
+				p++;
+			}
+			_putchar('\n');
+			h++;
+		}
+	}
+	else
+		_putchar('\n');
+}
