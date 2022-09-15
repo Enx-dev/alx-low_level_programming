eb8e05156793d72d2342650a4f64bff694a38598
Adejori Eniola <enx.co21@gmail.com>
2022-09-15 06:06:18 -0700

    update

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* print_numbers - prints all numbers from 0 to 9
+* Return: 0
+*/
+
+void print_numbers(void)
+{
+	int start = 0;
+
+	while (start <= 9)
+	{
+		_putchar(start + '0');
+		start++;
+	}
+	return (0);
+}
