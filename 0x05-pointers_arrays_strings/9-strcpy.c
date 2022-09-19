4eadccf1a142dfb47125103d15b52c194ac6e065
Adejori Eniola <enx.co21@gmail.com>
2022-09-19 09:41:45 -0700

    update

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -3,26 +3,20 @@
 /**
 * _strncpy - cpoies n bytes of src to the dest string
 * @dest: string to copy to
-* @src: string to copy from 
-* @n: bytes to copy
-*
+* @src: string to copy
 * Return: address of dest
 */
 
-char *_strncpy(char *dest, char *src, int n)
+char *_strncpy(char *dest, char *src)
 {
 	int i;
 
 	i = 0;
-	while (i < n && *(src + i))
+	while (*(src + i))
 	{
 		*(dest + i) = *(src + i);
 		i++;
 	}
-	while (i < n)
-	{
-		*(dest + i) = '\0';
-		i++;
-	}
+	*(dest + i) = '\0';
 	return (dest);
 }
