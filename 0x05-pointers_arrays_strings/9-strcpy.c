2d84e86e175073a8e696f3f675d59c3c7de56ecf
Adejori Eniola <enx.co21@gmail.com>
2022-09-19 09:57:20 -0700

    update

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,17 +1,16 @@
 #include "main.h"
 
 /**
-* _strncpy - cpoies n bytes of src to the dest string
+* _strcpy - cpoies n bytes of src to the dest string
 * @dest: string to copy to
 * @src: string to copy
 * Return: address of dest
 */
 
-char *_strncpy(char *dest, char *src)
+char *_strcpy(char *dest, char *src)
 {
-	int i;
+	int i = 0;
 
-	i = 0;
 	while (*(src + i))
 	{
 		*(dest + i) = *(src + i);
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -3,7 +3,7 @@
 
 #include <stdio.h>
 
-int _putchar(char c);
+int _putchar(char);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -13,5 +13,7 @@ void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
-char *_strcpy(char *dest, char *src);
+char * _strcpy(char *dest, char *src);
+int _atoi(char *s);
+
 #endif /* MAIN_H */
