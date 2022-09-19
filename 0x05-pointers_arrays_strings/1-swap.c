f0069ca812f621d39ec7cb4dfc83fa4636106c58
Adejori Eniola <enx.co21@gmail.com>
2022-09-19 05:49:53 -0700

    update

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* swap_int - Swaps value of a and b
+* @a: first number
+* @b: second number
+* Retutn: void
+*/
+
+void swap_int(int *a,int *b)
+{
+	int temp = *a;
+	 *a = *b;
+	 *b = temp;
+}
