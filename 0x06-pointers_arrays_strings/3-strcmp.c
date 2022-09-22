f5fd325bf015ab538a7ca0531cb3289775b688c0
Adejori Eniola <enx.co21@gmail.com>
2022-09-22 03:19:26 -0700

    update

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+* _strcmp - compares two string
+* @s1: frist string
+* @s2 seond string
+*
+* Return: int
+*/
+
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+
+	while (*(s1 + i) || *(s2 + i))
+	{
+		if (s1[i] == s2[i])
+		{
+			i++;
+			continue;
+		}
+		else
+			return (s1[i] - s2[i]);
+		
+	}
+	return (0);
+}
