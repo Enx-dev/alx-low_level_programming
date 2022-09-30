2fa39b7ac6c6c838795418233c384eea127df945
Adejori Eniola <enx.co21@gmail.com>
2022-09-30 06:31:54 -0700

    update

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* main - prints file name
+* @argc: amount of aguremnts passed
+* @argv :array of agurements
+*
+* Return: int
+*/
+
+int main(int argc, char  __attribute__((__unused__)) *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
