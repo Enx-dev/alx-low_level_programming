30f0b092bc86fb563c9b61e317412ebf5bc420c4
Adejori Eniola <enx.co21@gmail.com>
2022-10-03 02:27:59 -0700

    update

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,6 +15,8 @@ char *create_array(unsigned int size, char c)
 	unsigned int i = 0;
 
 	p = malloc(sizeof(char) * size);
+	if (size == 0)
+		return (NULL);
 	while (i < size)
 	{
 		*((char *)p + i) = c;
