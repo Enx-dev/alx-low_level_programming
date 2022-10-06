6de3e8ddafb925e0e7fa32504ea820552d486016
Adejori Eniola <enx.co21@gmail.com>
2022-10-06 14:56:21 +0100

    task 3 done

---
+++ 2-calloc.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _calloc - allocates memory for an array, using malloc.
+ * @nmemb: number of elements in the array
+ * @size: size in bytes of the elements
+ *
+ * Return: void pointer to allocated memory
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *p;
+	unsigned int i = 0;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	p = malloc(nmemb * size);
+	if (p == NULL)
+		return (NULL);
+	while (i < (nmemb * size))
+	{
+		p[i] = 0;
+		i++;
+	}
+	return (p);
+}
