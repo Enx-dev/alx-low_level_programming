a8cc122b4f792d68b7981912fb77f0ff740efea9
Adejori Eniola <enx.co21@gmail.com>
2022-10-06 06:39:43 -0700

    update

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+* malloc_checked - allocates memory 
+* @b: interger
+*
+* Return: void pointer
+*/
+
+void *malloc_checked(unsigned int b)
+{
+	void *p = NULL;
+
+	p = malloc(b);
+	if (p == NULL)
+	{
+		exit(98);
+	}
+	return (p);
+}
