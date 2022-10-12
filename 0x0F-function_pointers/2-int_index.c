266f3150689366c70655d408538e49c5809e8f94
Adejori Eniola <enx.co21@gmail.com>
2022-10-12 08:36:07 -0700

    update

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -13,7 +13,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 	if (size <= 0)
 		return (-1);
-	if (array != NULL && size > 0 && cmp != NULL)
+	if (array != NULL && cmp != NULL)
 		while (i < size)
 		{
 			if ((*cmp)(*(array + i)) != 0)
