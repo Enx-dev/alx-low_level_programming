b662701e54798f4538f1e5226038edbd14556480
Adejori Eniola <enx.co21@gmail.com>
2022-10-12 08:06:53 -0700

    update

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,3 +1,5 @@
+#include "function_pointers.h"
+
 /**
 * print_name - prints name with f function
 * @name: name to print
@@ -6,5 +8,6 @@
 
 void print_name(char *name, void (*f)(char *))
 {
-	f(name);
+	if (name != NULL && f != NULL)
+		f(name);
 }
