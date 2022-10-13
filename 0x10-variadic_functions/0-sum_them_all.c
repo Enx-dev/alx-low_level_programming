1c9285dbce8b8aec2707609298728c6db98ce500
Adejori Eniola <enx.co21@gmail.com>
2022-10-13 14:39:34 +0100

    test

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,14 @@
+#include "varidic_functions.h"
+
+/**
+* sum_them_all - finds the sum of all numbers passed
+* @n: size of agurements
+*
+* Return: sum of all agurements
+*/
+
+int sum_them_all(const unsigned int n, ...)
+{
+    va_list args;
+
+}
