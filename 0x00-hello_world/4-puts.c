7388546bac4a070caea3e1e74f1cdbf47dc8a038
Adejori Eniola <enx.co21@gmail.com>
2023-07-13 09:37:22 -0700

    i think am done

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
-/*
- * main:a function to print a string
+/**
+ * main - a function that prints a string
+ *
+ * Return: an int
  */
-
-int main() {
-	char str;
-
-	str = "\"Programming is like building a multilingual puzzle";
-	puts(str)
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints a string
+ *
+ * Return: an int
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
