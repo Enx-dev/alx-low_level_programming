bdc3679923b4dd1ba60a3b910128959a3c63a3f6
Adejori Eniola <enx.co21@gmail.com>
2023-07-18 08:36:47 -0700

    iwo lo wa

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -5,11 +5,19 @@
  */
 void jack_bauer(void)
 {
-	int a1, a2, b1, b2;
+	int a1, a2, b1, b2, limit;
 
 	for (a1 = 0; a1 < 3; a1++)
 	{
-		for (a2 = 0; a2 < 4; a2++)
+		if (a1 == 2)
+		{
+			limit = 4;
+		}
+		else
+		{
+			limit = 10;
+		}
+		for (a2 = 0; a2 < limit; a2++)
 		{
 			for (b1 = 0; b1 < 6; b1++)
 			{
