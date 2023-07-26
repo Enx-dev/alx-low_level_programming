e1cc6e9b974e0d8376426c3041b95f678cd02036
Enx-Dev <enx.co21@gmail.com>
2023-07-26 17:55:54 +0100

    test betty

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+/**
+ * _strcat - Concats @src to @dest 
+ * @src: string to be concatinated
+ * @dest: string to be added to
+ * 
+ * Return: a pointer to the resulting string
+*/
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	i = 0;
+	j = 0;
+	while (*(dest + i) != '\0')
+	{
+		i++;
+	}
+	while (*(src + j) != '\0')
+	{
+		*(dest + i) = *(src + j);
+		i++;
+		j++;
+	}
+	*(dest + i) = '\0';
+	return (dest);
+}
