5bacbd4ef9f8631ab7cdb5aac612720cbfe92093
Enx-Dev <enx.co21@gmail.com>
2023-08-04 15:26:03 +0100

    test 2

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -1,17 +1,4 @@
-#include "main.h"
-
-/**
- * _isupper - a function that checks if c is pointing to an uppercase
- * alphabet
- * @c: char with this ascii code will be checked
- *
- * Return: 1 if upper and 0 if not
- */
 int _isupper(int c)
 {
-	if (c >= 65 && c <= 90)
-	{
-		return (1);
-	}
-	return (0);
+	
 }
