a1cb7b0afacd8f5e904bde49442b4b9b80696f90
Adejori Eniola <enx.co21@gmail.com>
2023-08-25 11:38:11 -0700

    tried

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * print_list - prints alist
+ * @h: head of the list
+ *
+ * Return: size of the list
+ */
+size_t print_list(const list_t *h)
+{
+	unsigned int count;
+	const list_t *ptr;
+
+	count = 0;
+	ptr = h;
+	while (ptr->next != NULL)
+	{
+		count++;
+		if (ptr->str == NULL)
+		{
+			printf("[0] (nil)");
+		}
+		else
+		{
+			printf("[%u] %s\n", ptr->len, ptr->str);
+		}
+		ptr = ptr->next;
+	}
+
+	return (count);
+}
